e2d166b88fbcae307ebb21f44b80ce694f5907ec
sambuite <murilosambuite@gmail.com>
2022-02-21 10:37:13 -0300

    Listed repositories with fetched data from github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,26 @@
 import { RepositoryItem } from './RepositoryItem';
+import { useEffect, useState } from 'react';
 
-const repository = {
-  name: 'unform',
-  description: 'Descrição do melhor repositório',
-  link: 'https://github.com/unform/unform',
-};
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+      .catch((error) => console.log('ERROR', error));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
